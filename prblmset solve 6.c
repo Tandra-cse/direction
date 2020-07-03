330e8cc2d8c7be1ce1a13902e0b565b8bf9bbdf9
Tandra-cse <66546519+Tandra-cse@users.noreply.github.com>
2020-07-03 13:35:54 +0600

    Add files via upload

---
+++ prblmset solve 6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int j;
+    scanf("%d",&j);
+    int fibo[j + 1];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int i = 2; i <= j; i++)
+        fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 10;
+    printf("%d\j", fibo[j]);
+    return 0;
+}
+
+//runtime complexity:O(n)
