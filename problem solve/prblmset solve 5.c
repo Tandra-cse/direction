7632cc858a805686bdde93fe508f2d8577ec5ef4
Tandra-cse <66546519+Tandra-cse@users.noreply.github.com>
2020-08-08 04:44:20 +0600

    Add files via upload

---
+++ problem solve/prblmset solve 5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+
+    int fibo[46],k,l;
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int l = 2; l <= 45; l++)
+        fibo[l] = fibo[l - 1] + fibo[l - 2];
+    scanf("%d",&k);
+    printf("%d",fibo[k]);
+
+    return 0;
+
+}
+//runtime complexity:O(n)
