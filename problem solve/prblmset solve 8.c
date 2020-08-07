a9a3b72fbdf07a746da1ab70e90b5e4d265f0235
Tandra-cse <66546519+Tandra-cse@users.noreply.github.com>
2020-08-08 04:45:59 +0600

    Add files via upload

---
+++ problem solve/prblmset solve 8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+    int  a, b,gcd, rem;
+    scanf("%d %d",&a,&b);
+    while(t!=0)
+    {
+        rem=a%b;
+        a=b;
+        b=rem;
+    }
+    gcd =a;
+    printf("%d\n",gcd);
+    return 0;
+}
+
+
+//run time complexity:O(logn)
